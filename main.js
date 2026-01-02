4f65020fe6b56506dc96b3c18f85564ceb998a09
sumi-ha <ksm.shp.tachyon@gmail.com>
2026-01-02 21:44:30 +0900

    fix delete process

--- main.js
+++ main.js
@@ -64,8 +64,27 @@ async function render() {
       <td class="px-4 py-2 text-left">${item.currency}</td>
       <td class="px-4 py-2 text-right">${item.amount}</td>
       <td class="px-4 py-2 text-right">${Math.round(item.yen).toLocaleString('ja-JP')}</td>
+      <td class="text-center">
+        <button class="delete-btn text-gray-500 hover:text-red-700">
+          <!-- Heroicons: mini/trash -->
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" viewBox="0 0 20 20" fill="currentColor" class="size-5">
+            <path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 0 0 6 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 1 0 .23 1.482l.149-.022.841 10.518A2.75 2.75 0 0 0 7.596 19h4.807a2.75 2.75 0 0 0 2.742-2.53l.841-10.52.149.023a.75.75 0 0 0 .23-1.482A41.03 41.03 0 0 0 14 4.193V3.75A2.75 2.75 0 0 0 11.25 1h-2.5ZM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4ZM8.58 7.72a.75.75 0 0 0-1.5.06l.3 7.5a.75.75 0 1 0 1.5-.06l-.3-7.5Zm4.34.06a.75.75 0 1 0-1.5-.06l-.3 7.5a.75.75 0 1 0 1.5.06l.3-7.5Z" clip-rule="evenodd" />
+          </svg>
+        </button>
+      </td>
       `;
     tbody.appendChild(tr);
+
+    // deleteボタン作成
+    const btn = tr.querySelector('button');
+    btn.addEventListener('click', e => {
+      e.stopPropagation(); // 念のため、行クリックとバッティング防止
+      if (!window.confirm('本当に削除しますか？')) return;
+
+      data.splice(index, 1); // データ削除
+      save();
+      render(); // 再描画
+    });
   });
 
   const total = converted.reduce((sum, item) => {
@@ -74,7 +93,6 @@ async function render() {
   const totalEl = document.getElementById('total');
   totalEl.textContent = Math.round(total).toLocaleString('ja-JP');
 }
-
 
 // create処理
 document.getElementById('add-form').addEventListener('submit', e => {
@@ -85,19 +103,6 @@ document.getElementById('add-form').addEventListener('submit', e => {
   save();
   render();
   e.target.reset();
-});
-
-// delete処理
-document.getElementById('rows').addEventListener('click', e => {
-  const tr = e.target.closest('tr');
-  if (!tr) return;
-
-  const index = Number(tr.dataset.index);
-  if (Number.isNaN(index)) return;
-
-  data.splice(index, 1); // 削除
-  save();
-  render();
 });
 
 //
